bf5d97dfc27f30825c3086f877a6ac84e05b6571
akumar0709 <67645056+akumar0709@users.noreply.github.com>
2020-07-06 00:49:35 +0530

    Add files via upload

--- js/default.js
+++ js/default.js
@@ -3,3 +3,15 @@ window.addEventListener("scroll",()=>{
     const totalHeight = document.body.scrollHeight-window.innerHeight;
     document.querySelector("#scroll-indicator").style.width= (window.scrollY/totalHeight * 100)+"%";
 })
+
+const menuIcon = document.querySelector(".menu-icon-container");
+const navMob = document.querySelector(".navigation-mobile");
+menuIcon.addEventListener("click",()=>{
+    navMob.style.height="100%";
+})
+const closeMenu = document.querySelectorAll(".closemenu");
+closeMenu.forEach(element=>{
+    element.addEventListener("click",()=>{
+        navMob.style.height="0";
+    });
+})
